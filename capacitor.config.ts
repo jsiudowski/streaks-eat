1528fcecbb81ed23b845e51eda4302c0507322ee
jsiudowski <jsiudowski25@jcu.edu>
2024-10-03 22:44:58 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Streaks Eat',
+  webDir: 'dist'
+};
+
+export default config;
