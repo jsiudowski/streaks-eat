ce5f784953768f9829406259f4ad4a22291dfbf1
Abedelhadi Tawil <123099569+Atawil25@users.noreply.github.com>
2024-10-10 11:51:31 -0400

    Map Key fix
    Used Ionitem so it would appear on android rather than using HTML

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonProgressBar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
-import { useParams } from 'react-router';
-import './Map.css'
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonItem,
+  IonList,
+} from '@ionic/react';
+import './Map.css';
 
 const Map: React.FC = () => {
   return (
@@ -14,6 +24,7 @@ const Map: React.FC = () => {
           <IonTitle>Map of John Carroll</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent className="ion-padding">
         {/* Image Section */}
         <IonCard className="ion-card-responsive img">
@@ -22,53 +33,39 @@ const Map: React.FC = () => {
 
         {/* Numbered List Section */}
         <h2>ACADEMIC & SERVICE BUILDINGS</h2>
-        <ol>
-          <li>Boler College of Business</li>
-          <li>Carroll Gate</li>
-          <li>
-            Dolan Center for Science & Technology
-          </li>
-          <li>
-            Grasselli Library
-          </li>
-          <li>Lombardo Student Center - Bookstore</li>
-          <li>Military Science ROTC</li>
-          <li>O’Malley Center</li>
-          <li>Rodman Hall - Office of Admission</li>
-          <li>Schott Dining Hall</li>
-          <li>St. Francis Chapel</li>
-          <li>St. Ignatius Hall - Saxby’s Coffee Shop</li>
-        </ol>
+        <IonList>
+          <IonItem>1. Boler College of Business</IonItem>
+          <IonItem>2. Carroll Gate</IonItem>
+          <IonItem>3. Dolan Center for Science & Technology</IonItem>
+          <IonItem>4. Grasselli Library</IonItem>
+          <IonItem>5. Lombardo Student Center - Bookstore</IonItem>
+          <IonItem>6. Military Science ROTC</IonItem>
+          <IonItem>7. O’Malley Center</IonItem>
+          <IonItem>8. Rodman Hall - Office of Admission</IonItem>
+          <IonItem>9. Schott Dining Hall</IonItem>
+          <IonItem>10. St. Francis Chapel</IonItem>
+          <IonItem>11. St. Ignatius Hall - Saxby’s Coffee Shop</IonItem>
+        </IonList>
 
         <h2>RESIDENCE HALLS</h2>
-        <ol start="12">
-          <li>Bernet Hall</li>
-          <li>Campion Hall</li>
-          <li>Dolan Hall - University Counseling Center</li>
-          <li>Hamlin Hall</li>
-          <li>
-            Millor Hall
-          </li>
-          <li>Murphy Hall - Student Health Center</li>
-          <li>
-            Pacelli Hall 
-          </li>
-          <li>Sutowski Hall</li>
-        </ol>
+        <IonList>
+          <IonItem>12.Bernet Hall</IonItem>
+          <IonItem>13. Campion Hall</IonItem>
+          <IonItem>14. Dolan Hall - University Counseling Center</IonItem>
+          <IonItem>15. Hamlin Hall</IonItem>
+          <IonItem>16. Millor Hall</IonItem>
+          <IonItem>17. Murphy Hall - Student Health Center</IonItem>
+          <IonItem>18. Pacelli Hall</IonItem>
+          <IonItem>19. Sutowski Hall</IonItem>
+        </IonList>
 
         <h2>ATHLETIC FACILITIES</h2>
-        <ol start="20">
-          <li>DeCarlo Varsity Center</li>
-          <li>
-            Athletic, Event & Wellness Center
-          </li>
-          <li>Johnson Natatorium</li>
-          <li>
-            Recreation Complex
-          </li>
-          <li>Short Family Tennis Center</li>
-          <li>Shula Stadium</li>
-        </ol>
+        <IonList>
+          <IonItem>20. DeCarlo Varsity Center</IonItem>
+          <IonItem>21. Athletic, Event & Wellness Center</IonItem>
+          <IonItem>22. Johnson Natatorium</IonItem>
+          <IonItem>23. Recreation Complex</IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
   );
