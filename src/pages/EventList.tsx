797f8ed8f36ca99b7eea0739d8a8bc05d3863ece
Abedelhadi Tawil <123099569+Atawil25@users.noreply.github.com>
2024-10-15 11:08:35 -0400

    Plus button fix

--- src/pages/EventList.tsx
+++ src/pages/EventList.tsx
@@ -1,27 +1,27 @@
 import { IonButtons, IonFab, IonFabButton, IonHeader, IonIcon, IonMenuButton, IonPage, IonRouterLink, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 import './EventList.css';
+import { add } from 'ionicons/icons';
 
 const EventList: React.FC = () => {
     return (
         <IonPage>
             <IonHeader>
-          <IonToolbar>
-            <IonButtons slot="start">
-              <IonMenuButton />
-            </IonButtons>
-            <IonTitle>List of Events</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonFab slot="fixed" horizontal="end" vertical="bottom">
-          <IonRouterLink href='/pages/EventCreation' routerLink="/pages/EventCreation">
-           <IonFabButton className="add event">
-              <IonFabButton>
-                <IonIcon name="add-outline" color="white" aria-label="add event"></IonIcon>
-              </IonFabButton>
-            </IonFabButton>
-           </IonRouterLink>
-        </IonFab>
+                <IonToolbar>
+                    <IonButtons slot="start">
+                        <IonMenuButton />
+                    </IonButtons>
+                    <IonTitle>List of Events</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+
+            <IonFab slot="fixed" horizontal="end" vertical="bottom">
+                <IonRouterLink routerLink="/pages/EventCreation">
+                    <IonFabButton className="add-event">
+                        <IonIcon icon={add} color="white" aria-label="add event" />
+                    </IonFabButton>
+                </IonRouterLink>
+            </IonFab>
         </IonPage>
     );
 }
